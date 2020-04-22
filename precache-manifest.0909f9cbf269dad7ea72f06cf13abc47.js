4221df0ae067e4020d3d515081009dba707258f9
Artem Shalamov <iart1756@gmail.com>
2020-04-22 11:36:07 +0500

    Updates

---
+++ precache-manifest.0909f9cbf269dad7ea72f06cf13abc47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c71bb2220a38f09dbe926e23fb9388",
+    "url": "/trainingAPIRecipe/index.html"
+  },
+  {
+    "revision": "9a2c306dcfcc16dfbddb",
+    "url": "/trainingAPIRecipe/static/css/main.86b531bb.chunk.css"
+  },
+  {
+    "revision": "4dbad627ec560caca196",
+    "url": "/trainingAPIRecipe/static/js/2.3353d83c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/trainingAPIRecipe/static/js/2.3353d83c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a2c306dcfcc16dfbddb",
+    "url": "/trainingAPIRecipe/static/js/main.c2acee73.chunk.js"
+  },
+  {
+    "revision": "ad6613422f73391922f7",
+    "url": "/trainingAPIRecipe/static/js/runtime-main.3444fe59.js"
+  }
+]);
